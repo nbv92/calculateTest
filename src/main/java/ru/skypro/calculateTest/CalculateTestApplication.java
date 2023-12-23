761cc8220ff165a509855832bd38fa316c46cbff
Timofey <timofey.sharipov@gmail.com>
2023-12-23 03:59:53 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/calculateTest/CalculateTestApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.calculateTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/calculateTest/CalculateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.calculateTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
